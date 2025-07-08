4f0bca1f50b3d8ceba09ecfce6ed8500958e0b7a
Zgplay056 <130008651+Zgplay056@users.noreply.github.com>
2025-07-07 23:06:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const botao = document.getElementById('botao')
+const alterar = document.getElementById('alterar')
+botao.addEventListener("click", function () {
+
+      const mensagem = "O esforço e a dedicação trazem frutos que o talento sozinho não consegue. Lee, Rock.";
+
+      alterar.textContent = mensagem
+});
